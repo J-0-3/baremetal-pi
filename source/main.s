b45b45c9c17bb1b58beb6dd88d43940d44d76203
joe <joedt100@gmail.com>
2021-12-27 12:12:52 +0000

    turn off red light as well

--- source/main.s
+++ source/main.s
@@ -2,9 +2,9 @@
 
 .section .data
 .align 4
-PropertyInfo:
+EnableGreen:
     //Message Header
-    .int PropertyInfoEnd-PropertyInfo
+    .int EnableGreenEnd-EnableGreen
     .int 0
     //Tag Header (0x00038041 = SET_GPIO_SET)
     .int 0x00038041
@@ -14,7 +14,20 @@ PropertyInfo:
     .int 29
     .int 1
     .int 0
-PropertyInfoEnd:
+EnableGreenEnd:
+
+.align 4
+EnableRed:
+     //Message Header
+     .int EnableRedEnd-EnableRed
+     .int 0
+     .int 0x00038041
+     .int 8
+     .int 0
+     .int 130
+     .int 1
+     .int 0
+EnableRedEnd:
 
 .section .text
 _start:
@@ -27,11 +40,18 @@ _start:
         bne wait1$
     //Load message data address into r1
     //(previous value not in use, save registers)
-    ldr r1, =PropertyInfo
+    ldr r1, =EnableGreen
     //8 = PropertyTag GPU channel
     add r1, #8
     //Write register for mailbox 1 is at 0x20 offset
     str r1, [r0, #0x20]
 
+    wait2$:
+	ldr r1, [r0, #0x38]
+	tst r1, #0x80000000
+	bne wait2$
+    ldr r1,=EnableRed
+    add r1, #8
+    str r1, [r0, #0x20]
     hang:
         b hang
